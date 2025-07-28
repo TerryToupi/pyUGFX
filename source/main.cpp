bce0129bb90f9325e32578badc979514cf3250fd
TerryToupi <terrytoupi@gmail.com>
2025-07-28 15:02:36 +0300

    Setting MSVC runtime to be static

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,17 @@
-#include <iostream> 
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <volk.h>
+#include <assert.h>
+#include <shaderc/shaderc.hpp>
 
 int main()
 {
+    shaderc::Compiler compiler;
+    shaderc::CompileOptions options;
+
+    glfwInit();
+    assert(volkInitialize() == VK_SUCCESS);
+
     std::cout << "Hello World!";
     return 0;
 }
