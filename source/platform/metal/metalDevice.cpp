a4e633d32856e2211cd62b451d505eedf04864d4
TerryToupi <terrytoupi@gmail.com>
2025-08-09 00:27:41 +0300

    first metal window

--- source/platform/metal/metalDevice.cpp
+++ source/platform/metal/metalDevice.cpp
@@ -1,10 +1,21 @@
 #include <metalDevice.hpp>
 #include <Metal.hpp>
 
+#include <GLFW/glfw3.h>
+
 void gfx::MetalDevice::Init()
 {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Heavy", NULL, NULL);
+    
     MTL::Device* device = MTL::CreateSystemDefaultDevice();
     CA::MetalLayer* layer = CA::MetalLayer::layer()->retain();
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
     
     layer->setDevice(device);
     
