47da726e9b61478e1f8de7997cf2ed9c3edfc30d
TerryToupi <terrytoupi@gmail.com>
2025-08-08 23:45:23 +0300

    Metal support and linking

--- source/platform/metal/metalDevice.cpp
+++ source/platform/metal/metalDevice.cpp
@@ -1,7 +1,14 @@
 #include <metalDevice.hpp>
+#include <Metal.hpp>
 
 void gfx::MetalDevice::Init()
 {
+    MTL::Device* device = MTL::CreateSystemDefaultDevice();
+    CA::MetalLayer* layer = CA::MetalLayer::layer()->retain();
+    
+    layer->setDevice(device);
+    
+    layer->release();
 }
 
 void gfx::MetalDevice::ShutDown()
