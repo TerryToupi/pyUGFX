14cfe4786c80ef90c79b26da7351fa3d6afa4d88
TerryToupi <terrytoupi@gmail.com>
2025-08-08 12:10:21 +0300

    added cpm for package caching

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -12,49 +12,49 @@ function(group_sources_by_folder_from_list file_list base_dir)
     endforeach()
 endfunction()
 
-# Wraps add_subdirectory and moves all created targets to "Import"
-function(add_subdirectory_import dir)
-    # Save list of existing targets
-    get_property(_before_targets GLOBAL PROPERTY TARGETS)
+# # Wraps add_subdirectory and moves all created targets to "Import"
+# function(add_subdirectory_import dir)
+#     # Save list of existing targets
+#     get_property(_before_targets GLOBAL PROPERTY TARGETS)
 
-    # Save important context variables
-    set(SAVED_CMAKE_FOLDER "${CMAKE_FOLDER}")
-    set(SAVED_CURRENT_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
-    set(SAVED_CURRENT_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+#     # Save important context variables
+#     set(SAVED_CMAKE_FOLDER "${CMAKE_FOLDER}")
+#     set(SAVED_CURRENT_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+#     set(SAVED_CURRENT_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}")
 
-    # Set a folder for grouping in IDEs like Visual Studio/Xcode
-    set(CMAKE_FOLDER "Import")
+#     # Set a folder for grouping in IDEs like Visual Studio/Xcode
+#     set(CMAKE_FOLDER "Import")
 
-    # Add the dependency
-    add_subdirectory(${dir} ${ARGN})
+#     # Add the dependency
+#     add_subdirectory(${dir} ${ARGN})
 
-    # Restore previous context
-    set(CMAKE_FOLDER "${SAVED_CMAKE_FOLDER}")
-    set(CMAKE_CURRENT_SOURCE_DIR "${SAVED_CURRENT_SOURCE_DIR}")
-    set(CMAKE_CURRENT_BINARY_DIR "${SAVED_CURRENT_BINARY_DIR}")
+#     # Restore previous context
+#     set(CMAKE_FOLDER "${SAVED_CMAKE_FOLDER}")
+#     set(CMAKE_CURRENT_SOURCE_DIR "${SAVED_CURRENT_SOURCE_DIR}")
+#     set(CMAKE_CURRENT_BINARY_DIR "${SAVED_CURRENT_BINARY_DIR}")
 
-    # Capture all new targets
-    get_property(_after_targets GLOBAL PROPERTY TARGETS)
-    list(REMOVE_ITEM _after_targets ${_before_targets})
+#     # Capture all new targets
+#     get_property(_after_targets GLOBAL PROPERTY TARGETS)
+#     list(REMOVE_ITEM _after_targets ${_before_targets})
 
-    # Apply "Import" folder to all new targets
-    foreach(target IN LISTS _after_targets)
-        if(TARGET ${target})
-            set_target_properties(${target} PROPERTIES FOLDER "Import")
-        endif()
-    endforeach()
-endfunction()
+#     # Apply "Import" folder to all new targets
+#     foreach(target IN LISTS _after_targets)
+#         if(TARGET ${target})
+#             set_target_properties(${target} PROPERTIES FOLDER "Import")
+#         endif()
+#     endforeach()
+# endfunction()
 
-# Manually mark a target as part of Import
-function(mark_target_import target_name)
-    if(TARGET ${target_name})
-        set_target_properties(${target_name} PROPERTIES FOLDER "Import")
-    endif()
-endfunction()
+# # Manually mark a target as part of Import
+# function(mark_target_import target_name)
+#     if(TARGET ${target_name})
+#         set_target_properties(${target_name} PROPERTIES FOLDER "Import")
+#     endif()
+# endfunction()
 
-# Mark multiple targets
-function(mark_targets_import)
-    foreach(name IN LISTS ARGN)
-        mark_target_import(${name})
-    endforeach()
-endfunction()
+# # Mark multiple targets
+# function(mark_targets_import)
+#     foreach(name IN LISTS ARGN)
+#         mark_target_import(${name})
+#     endforeach()
+# endfunction()
